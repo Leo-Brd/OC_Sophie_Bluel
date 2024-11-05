17aa3872d5d2cfc86766d2210567ca9694ffecce
Leo-Brd <leobordet.pro@gmail.com>
2024-11-05 17:25:11 +0100

    add generateWorks function

--- FrontEnd/scripts/mainPage.js
+++ FrontEnd/scripts/mainPage.js
@@ -1,13 +1,39 @@
 
-let works = window.localStorage.getItem('works');
+function generateWorks(works) {
 
-if (works === null) {
-    const reponse = await fetch('http://localhost:5678/api/works');
-    works = await reponse.json();
+    for (let i = 0; i < works.length; i++) {
 
-    const valeurWorks = JSON.stringify(works);
+        const work = works[i];
 
-    window.localStorage.setItem("works", valeurWorks);
-} else {
-    works = JSON.parse(works);
+        const gallery = document.querySelector(".gallery");
+        const figureBalise = document.createElement("figure");
+
+        const imageElement = document.createElement("img");
+        imageElement.src = work.imageUrl;
+        const titreElement = document.createElement("figcaption");
+        titreElement.innerText = work.title;
+
+        figureBalise.appendChild(imageElement);
+        figureBalise.appendChild(titreElement);
+        gallery.appendChild(figureBalise);
+    }
 }
+
+async function fetchWorks() {
+    let works = window.localStorage.getItem('works');
+
+    if (works === null) {
+        const reponse = await fetch('http://localhost:5678/api/works');
+        works = await reponse.json();
+
+        const valeurWorks = JSON.stringify(works);
+        window.localStorage.setItem("works", valeurWorks);
+    } else {
+        works = JSON.parse(works);
+    }
+
+    generateWorks(works);
+}
+
+
+fetchWorks().catch(error => console.error("Erreur :", error));
