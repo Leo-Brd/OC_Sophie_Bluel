e875320878407e13aeba471866b478360dd75e6d
Leo-Brd <leobordet.pro@gmail.com>
2024-11-22 11:47:47 +0100

    delete localstorage using at loading to fix broken filters

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -3,18 +3,20 @@ import { checkAuthentication } from "./login.js"
 import { modal } from "./modal.js"
 
 async function fetchWorks() {
-    let works = window.localStorage.getItem('works');
 
-    if (works === null) {
-        const reponse = await fetch('http://localhost:5678/api/works');
-        works = await reponse.json();
+    try {
+        const response = await fetch('http://localhost:5678/api/works');
+        if (!response.ok) {
+            throw new Error('Erreur lors de la récupération des works');
+        }
+        const works = await response.json();
 
-        const valeurWorks = JSON.stringify(works);
-        window.localStorage.setItem("works", valeurWorks);
-    } else {
-        works = JSON.parse(works);
+        localStorage.setItem('works', JSON.stringify(works));
+        generateGallery(works);
+    } catch (error) {
+        console.error('Erreur lors du chargement des works :', error);
     }
-    generateGallery(works);
+    
 }
 
 
