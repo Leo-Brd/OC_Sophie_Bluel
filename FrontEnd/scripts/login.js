e9167d385e7eecf93d819a9c9b496edaddaa6400
Leo-Brd <leobordet.pro@gmail.com>
2024-12-03 15:49:36 +0100

    Improve login.js functions

--- FrontEnd/scripts/login.js
+++ FrontEnd/scripts/login.js
@@ -1,12 +1,12 @@
 
 /* when login success, we redirect to main page and add the token */
-export function onLoginSuccess(data) {
+function onLoginSuccess(data) {
     localStorage.setItem("authToken", data.token);
     window.location.href = "index.html";
 }
 
 /* when login fail, we print an error message */
-export function onLoginError() {
+function onLoginError() {
     const form = document.getElementById("login");
 
     let errorMessage = document.querySelector(".login-error");
@@ -21,41 +21,47 @@ export function onLoginError() {
 
 
 /* we send our login data to API and process the answer */
-export async function dataProcess(loginData) {
+async function dataProcess(loginData) {
 
-    fetch('http://localhost:5678/api/users/login', {
-        method: 'POST',
-        headers: { 
-            'Accept': 'application/json',
-            'Content-Type': 'application/json' 
-        },
-        body: JSON.stringify(loginData)
-    })
-    .then(answer => answer.json())
-    .then(data => {
+    try {
+        const response = await fetch('http://localhost:5678/api/users/login', {
+            method: 'POST',
+            headers: { 
+                'Accept': 'application/json',
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(loginData)
+        })
+        const data = await response.json();
+    
         if (data.token) {
             onLoginSuccess(data);
         } else {
             onLoginError();
         }
-    })
-    .catch(error => console.error('Erreur :', error));
+    } catch (error) {
+        console.error('Erreur lors du traitement des données du formulaire :', error);
+    }
 }
 
 /* we get the datas from the form */
-export async function listenLogin() {
+async function listenLogin() {
 
     const form = document.getElementById("login");
 
     form.addEventListener('submit', (event)=> {
         event.preventDefault();
 
-        const loginData = {
-            email: document.querySelector('#email').value,
-            password: document.querySelector('#password').value
-        };
+        try {
+            const loginData = {
+                email: document.querySelector('#email').value,
+                password: document.querySelector('#password').value
+            };
+            dataProcess(loginData);
 
-        dataProcess(loginData);
+        } catch (error) {
+            console.error('Erreur lors de la récupération des valeurs du formulaire :', error);
+        }
     })
 }
 
@@ -69,24 +75,28 @@ document.addEventListener('DOMContentLoaded', ()=> {
 /***** Functions for the main page *****/
 
 /* show more elements when user is connected */
-export function showLoggedInUI() {
+function showLoggedInUI() {
 
-    const loginHeader = document.getElementById("login-header");
-    loginHeader.style.display = "flex";
+    try {
+        const loginHeader = document.getElementById("login-header");
+        loginHeader.style.display = "flex";
 
-    const logoutButton = document.getElementById("login-logout");
-    logoutButton.innerHTML = "logout";
-    logoutButton.href = "#";
-    logoutButton.addEventListener("click", ()=> {
-        localStorage.removeItem("authToken");
-        window.location.href = "index.html";
-    });
+        const logoutButton = document.getElementById("login-logout");
+        logoutButton.innerHTML = "logout";
+        logoutButton.href = "#";
+        logoutButton.addEventListener("click", ()=> {
+            localStorage.removeItem("authToken");
+            window.location.href = "index.html";
+        });
 
-    const portfolioButton = document.querySelector(".portfolio-title button");
-    portfolioButton.style.display = "flex";
+        const portfolioButton = document.querySelector(".portfolio-title button");
+        portfolioButton.style.display = "flex";
 
-    const filtersButtons = document.querySelector(".filters");
-    filtersButtons.style.display = 'none';
+        const filtersButtons = document.querySelector(".filters");
+        filtersButtons.style.display = 'none';
+    } catch (error) {
+        console.error('Erreur lors de l\'affichage de l\'interface du mode connecté :', error);
+    }
 
 }
 
@@ -95,6 +105,7 @@ export function checkAuthentication() {
     const token = localStorage.getItem("authToken");
     if (token) {
         showLoggedInUI();
+        console.log("Utilisateur connecté.");
     } else {
         console.log("Aucun utilisateur connecté.");
     }
